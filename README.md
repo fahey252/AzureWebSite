b6f7218e9f6573ce22e41b1a328ccacac1ae4709
Fahey <fahey252@gmail.com>
2016-04-30 19:57:41 -0400

    Added sample for incoming email

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AzureWebSite
-Sample website deployed to Azure.
+Training website deployed to Azure.  <http://fahey.azurewebsites.net>
+
+# Deployment
+  * Do a git push.
+
+##Links
+  * <http://fahey.azurewebsites.net>
+  * <https://cfahey@fahey.scm.azurewebsites.net/Fahey.git>
+  * <https://github.com/fahey252/AzureWebSite.git>
--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
+var incomingMail = require('./routes/incomingMail');
 
 var app = express();
 
@@ -17,21 +16,24 @@ app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // view engine setup
-
+/*
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+*/
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}));    // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
+/*
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/incomingMail', incomingMail);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +41,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+*/
 
 /// error handlers
 
@@ -65,6 +68,21 @@ app.use(function(err, req, res, next) {
         error: {},
         title: 'error'
     });
+});
+
+app.get('/', function (request, response) {
+    console.log(request.route);
+    response.json("{home: true}");
+});
+
+app.get('/incomingMail', function (request, response) {
+    console.log(request.route);
+    response.json("{incomingMail: true}");
+});
+
+app.post('/incomingMail', function (req, res, next) {
+  console.log('Incoming Mail', req.body);
+  res.json(req.body);
 });
 
 
